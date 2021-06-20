eafaaefa928ebfb2882eff9a7495fbec891026ed
NotSugden <28943913+NotSugden@users.noreply.github.com>
2021-06-20 19:55:24 +0100

    fix: frozen movement after meetings

---
+++ source/Patches/MovementPatch.cs
@@ -0,0 +1,14 @@
+﻿using HarmonyLib;
+
+namespace TownOfUs
+{
+    [HarmonyPatch(typeof(ControllerManager), nameof(ControllerManager.IsUiControllerActive), MethodType.Getter)]
+    public static class MovementPatch
+    {
+        public static bool Prefix(ControllerManager __instance, out bool __result)
+        {
+            __result = false;
+            return false;
+        }
+    }
+}
